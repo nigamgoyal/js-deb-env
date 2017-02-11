40ce4b58d44a0130c416c961bcb880648c5690dc
Nigam Goyal <nigamgoyal@gmail.com>
2017-02-12 00:38:53 +0530

    Making HTTP Calls, Mocking HTTP Calls, JSON Schema faker

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,12 @@ app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users',function(req,res){
+	res.json([
+		{"id":1, "firstName":"Bob","lastName":"smith","email":"bob@gmail.com"},
+		{"id":2,"firstName":"Tammy","lastName":"Nortan","email":"tammy@yahoo.in"}
+	]);
+});
 
 app.listen(port,function(err){
 	if(err){
